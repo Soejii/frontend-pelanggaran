2ba670547ef602dcf40556f181e68b002652771f
Soejii <ninkaishou27@gmail.com>
2022-05-17 07:12:49 +0700

    Twelfth Commit,Added Nama and Poin Siswa in ListPelanggaran

--- src/pages/ListPelanggaranSiswa.js
+++ src/pages/ListPelanggaranSiswa.js
@@ -27,12 +27,31 @@ export default function ListPelanggaranSiswa(){
     return (
         <div className="container-fluid">
             <div className="card">
-                <div className="card-header"
+                <div className="ca item =>rd-header"
                 style={{background : 'salmon'}}>
                     <h4 className="text-white" >
                         List Pelanggaran Siswa
                     </h4>
                 </div>
+            </div>
+            <div className="card-body">
+                <ul className="list-group">
+                    {list.map(item => (
+                        <li className="list-group-item"
+                        key={`idPelanggaran${item.id_pelanggaran_siswa}`}>
+                            <div className="row">
+                                <div className="col-4">
+                                    <small>Nama Siswa</small>
+                                    <h5>{item.siswa.nama}</h5>
+                                </div>
+                                <div className="col-2">
+                                    <small>Poin Siswa</small>
+                                    <h5>{item.siswa.poin}</h5>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
